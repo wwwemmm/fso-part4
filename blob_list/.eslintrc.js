1e7b0ad62856273125a378eb97d68d9e07c355fe
Wen Meiqi <896974316@qq.com>
2023-08-16 11:24:37 +0800

    Ex4.1-4.7

--- blob_list/.eslintrc.js
+++ blob_list/.eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   'env': {
     'node': true,
     'commonjs': true,
-    'es2021': true
+    'es2021': true,
+    'jest': true,
   },
   'extends': 'eslint:recommended',
   'overrides': [
     {
       'env': {
-        'node': true
+        'node': true,
       },
       'files': [
         '.eslintrc.{js,cjs}'
